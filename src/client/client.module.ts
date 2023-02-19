76ec822dcafa1ad4dc1b5d8ba12945f3fa25c103
Asadbek <devasadbek@gmail.com>
2023-02-19 14:37:02 +0500

    fixed

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { OrdersModule } from 'src/orders/orders.module';
 import { ServicesModule } from 'src/services/services.module';
 import { ClientService } from './client.service';
 import { Client, ClientSchema } from './schemas/client.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Client.name, schema: ClientSchema }]), ServicesModule],
-  providers: [ClientService]
+  imports: [MongooseModule.forFeature([{ name: Client.name, schema: ClientSchema }]), ServicesModule, OrdersModule],
+  providers: [ClientService],
+  exports: [ClientService],
 })
 export class ClientModule {}
